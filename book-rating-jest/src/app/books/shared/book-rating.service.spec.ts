92c75544ac5a80d9322c3c91c02a3c5614364777
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-07-08 14:01:48 +0200

    testing: isolierter Unit Test

--- book-rating-jest/src/app/books/shared/book-rating.service.spec.ts
+++ book-rating-jest/src/app/books/shared/book-rating.service.spec.ts
@@ -5,14 +5,45 @@ import { BookRatingService } from './book-rating.service';
 
 describe('BookRatingService', () => {
   let service: BookRatingService;
+  let book: Book;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BookRatingService);
+    service = new BookRatingService();
+
+    book = {
+      isbn: '',
+      title: '',
+      rating: 3,
+      description: '',
+      authors: [],
+      firstThumbnailUrl: null
+    };
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should rate up a book by one [JIRA-1231]', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(4);
   });
 
+  it('should rate down a book by one', () => {
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('it should not rate higher than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('it should not rate lower than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('it should respect immutability and return a new book', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook).not.toBe(book);
+  });
 });
