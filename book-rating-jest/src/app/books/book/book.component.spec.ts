65eb86ac903b2ee1431af47ede1032bb8736f3de
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-07-08 14:54:02 +0200

    testing: observables (don't make them async) + launch.json

--- book-rating-jest/src/app/books/book/book.component.spec.ts
+++ book-rating-jest/src/app/books/book/book.component.spec.ts
@@ -1,6 +1,7 @@
 import { Directive, Input, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Book } from '../shared/book';
 import { RepeatDirective } from '../shared/repeat.directive';
 
 import { BookComponent } from './book.component';
@@ -35,11 +36,23 @@ describe('BookComponent', () => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
 
+    // ggf. Book bereitstellen, sofern notwendig
+    // component.book = {}
+
     fixture.detectChanges();
   });
 
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit the rateDown event on doRateDown()', () => {
+
+    let emittedBook: Book | null = null;
+    component.book = {} as Book;
+    component.rateDown.subscribe(e => {
+      emittedBook = e;
+    });
+
+    component.doRateDown();
+
+    expect(emittedBook).toBeTruthy();
   });
 });
