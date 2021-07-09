32ca839c9b69e5a559fa059551c0201b845a9405
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-07-09 12:01:31 +0200

    testing: http (own mock)

--- book-rating-jest/src/app/books/shared/book-store.service.stub.spec.ts
+++ book-rating-jest/src/app/books/shared/book-store.service.stub.spec.ts
@@ -1,19 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Book } from './book';
 
 import { BookStoreService } from './book-store.service';
 
 describe('BookStoreService with HTTP stub', () => {
   let service: BookStoreService;
+  let httpMock: Partial<HttpClient>;
+  const dummyBook = {
+    isbn: '000',
+    title: 'Angular'
+  };
 
   beforeEach(() => {
+    httpMock = {
+      get: jest.fn().mockReturnValue(of(dummyBook))
+    };
+
     TestBed.configureTestingModule({
-      providers: []
+      providers: [{
+        provide: HttpClient,
+        useValue: httpMock
+      }]
     });
     service = TestBed.inject(BookStoreService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should call the correct URL for getSingle', () => {
+    service.getSingle('123');
+    expect(httpMock.get).toHaveBeenCalledWith('https://api.angular.schule/books/123');
+  });
+
+  it('should provide the requested book', () => {
+    let result: Book | null = null;
+    service.getSingle('123').subscribe(book => result = book);
+
+    expect(result).toEqual(dummyBook);
   });
 
 });
